33c9e310bf0e3916214d26c67ba387a070ea63e2
Abdiomar Uldana <Abdiumar@list.ru>
2024-10-30 16:33:34 +0500

    Exercise

---
+++ 30.10/exercise.js
@@ -0,0 +1,8 @@
+let notebookName = "Acer";
+let processor = "Intel core 5";
+let size = "15.5";
+let color =  "black";
+let todayWeather = "cloud";
+let currentTime = "16:32";
+
+console.log ("My Laptop", notebookName )     
---
+++ 30.10/prac.js
@@ -0,0 +1,9 @@
+    let name = "Uldana";
+    let surname = "Abdiomar";
+    let birthday = "01.07.2008";
+    let school = "ZHKOM";
+    let weight = "60kg";
+
+console.log ("My name:", name);
+console.log ("My surname:" , surname)
+console.log ("My birthday" , birthday)

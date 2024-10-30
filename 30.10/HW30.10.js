4ba70d51d34009e250bd546396c3cecc1765dc8c
Abdiomar Uldana <Abdiumar@list.ru>
2024-10-30 21:32:47 +0500

    Home Work 30.10

---
+++ 30.10/HW30.10.js
@@ -0,0 +1,14 @@
+    let modelName = "Matiz";
+    let color = "greenyellow";
+    let wheelsCount = 4;
+
+    let  rating = 59; 
+
+
+
+    console.log("modelName:" , modelName)
+    console.log(" color:", color)
+    console.log(" wheelsCount:", wheelsCount)
+    console.log(" rating:" , rating)
+   
+    

5ea8c713e3c1c96af357b65b57c3d551d72044d9
Abdiomar Uldana <Abdiumar@list.ru>
2024-10-31 10:50:07 +0500

    30.10 Home work

--- 30.10/HW30.10.js
+++ 30.10/HW30.10.js
@@ -1,14 +1,12 @@
-    let modelName = "Matiz";
-    let color = "greenyellow";
-    let wheelsCount = 4;
-
-    let  rating = 59; 
-
-
-
-    console.log("modelName:" , modelName)
-    console.log(" color:", color)
-    console.log(" wheelsCount:", wheelsCount)
-    console.log(" rating:" , rating)
+let modelName = "Matiz";
+let color = "greenyellow";
+let wheelsCount = 4;
+let  isOffRoad  = "true";
+let  rating = 59; 
+console.log("modelName:" , modelName)
+console.log(" color:", color)
+console.log(" wheelsCount:", wheelsCount)
+console.log(" isOffRoad:" ,  isOffRoad)
+console.log(" rating:" , rating)
    
     

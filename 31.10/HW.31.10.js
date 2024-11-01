7c473f6eda3cf5ca38572b5f989f87a4bad89f58
Abdiomar Uldana <Abdiumar@list.ru>
2024-11-01 15:32:17 +0500

    Home work 31.10

---
+++ 31.10/HW.31.10.js
@@ -0,0 +1,23 @@
+// exercise 1
+let slicesPerPizza = 8;
+let numberOfPizza = 5;
+
+let totalSlices = slicesPerPizza * numberOfPizza;
+
+console.log ("totalSlices:" ,  totalSlices );
+
+// exercise 2
+
+let totalChocolates = 24;
+let numberOfFriends = 6;
+
+let chocolatesPerFriend = totalChocolates / numberOfFriends ;
+
+console.log(" chocolatesPerFriend:",  chocolatesPerFriend);
+
+
+// exercise 3
+let  plantHeight = 10;
+plantHeight += 2;
+
+console.log (" plantHeight:", plantHeight);

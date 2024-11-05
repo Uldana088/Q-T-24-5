d3f2c6a2d648643dcd6853f8ae9470464e953c52
Abdiomar Uldana <Abdiumar@list.ru>
2024-11-05 14:32:15 +0500

    Home work 04.11

---
+++ 04,11/Hw.js
@@ -0,0 +1,34 @@
+// exercise  1
+
+var age = 14;
+console.log(age > 16);
+
+
+// exercise 2
+
+
+var num2 = 22;
+if(num2 % 2 == 0 ){
+    console.log("жұп");
+}else{
+    console.log("тақ");
+}
+
+
+// exercise  3
+
+let temperature = 25;
+console.log(temperature > 20 && temperature < 30);
+// task 4
+let num4 = -1;
+console.log(num4 > 0);
+
+
+
+// exercise  5
+let num5 = 15;
+if (num5 % 5 == 0 && num5 % 10 == 0) {
+  console.log("true");
+} else {
+  console.log("false");
+}
